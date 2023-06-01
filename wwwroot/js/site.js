203c2648ab1f6c3b4674b7693a50825824cd073c
Luiz <luizbr080604@gmail.com>
2023-06-01 12:29:12 -0300

    Atualizacao Noticia sobre e contato

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,14 @@
 ﻿
+var itensSelecionados = document.querySelectorAll(".menu-item  a");
+
+itensSelecionados.forEach(function (item) {
+    item.addEventListener("click", function (event) {
+    itensSelecionados.forEach(function(itemSelecionado) {
+        itemSelecionado.style.borderBottom = "none";
+        itemSelecionado.style.color = "white";
+    });
+        item.style.color = "yellow";
+    item.style.paddingBottom = "35px";
+    item.style.borderBottom = "2px solid yellow"; 
+  });
+});
